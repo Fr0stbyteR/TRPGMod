9f00c726d608a7e8e26c01be3ca165495d2995d3
Fr0stbyteR <renshihong@hotmail.com>
2019-05-30 21:34:13 +0200

    Echo dice roll result

--- src/main/java/net/fr0stbyter/trpgmod/network/message/MessageRollDiceHandler.java
+++ src/main/java/net/fr0stbyter/trpgmod/network/message/MessageRollDiceHandler.java
@@ -1,5 +1,8 @@
 package net.fr0stbyter.trpgmod.network.message;
 
+import net.fr0stbyter.trpgmod.util.Dice.EnumDiceType;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
@@ -7,8 +10,12 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 public class MessageRollDiceHandler implements IMessageHandler<MessageRollDice, IMessage> {
     @Override
     public IMessage onMessage(MessageRollDice message, MessageContext ctx) {
-        int rolled = (int) (Math.floor(Math.random() * message.type.range) + 1);
-        System.out.println(rolled);
+        EnumDiceType diceType = message.type;
+        int rolled = (int) (Math.floor(Math.random() * diceType.range) + 1);
+        EntityPlayerMP player = ctx.getServerHandler().player;
+        String playerName = player.getName();
+        String text = playerName + " rolled 1" + diceType.name() + ": " + rolled;
+        ctx.getServerHandler().player.sendMessage(new TextComponentString(text));
         return null;
     }
 }
